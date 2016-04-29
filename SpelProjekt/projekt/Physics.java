7160fd0d9d0baf4c078617d7f344a9537eb699f8
AlbinAndersson96 <albin-andersson1996@hotmail.com>
2016-04-29 13:57:17 +0200

    Started Physics class

---
+++ SpelProjekt/projekt/Physics.java
@@ -0,0 +1,29 @@
+package projekt;
+
+import java.lang.*;
+
+public class Physics 
+{
+	double g = 9.82;
+	
+	double calcVelocity()
+	{
+		double v = 0;
+		return v;
+	}
+	
+	double calcPos(double x, double y, double v, double degrees, double dt)
+	{
+		double YVelocity = v * Math.sin(Math.toRadians(degrees));
+		
+		return 0;
+	}
+	
+	double calcFeatherVel(double c, double x, double y)
+	{
+		double v = 0;
+		return v;
+	}
+}
+
+
